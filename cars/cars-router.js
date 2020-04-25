4ccbd0310758e52940bf8d1b5aee853005aa822d
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2020-04-25 11:46:08 -0700

    More stretch complete: Update and Delete endpoints are functional.

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -28,4 +28,37 @@ router.post('/', async (req, res, next) => {
     }
 })
 
+//DELETE
+router.delete('/:id', async (req, res, next) => {
+	try {
+        await db("cars").where("id", req.params.id).del()
+        res.status(204).end()
+	}
+	catch (err) {
+		next(err)
+	}
+})
+
+//UPDATE
+router.put('/:id', async (req, res, next) => {
+    try {
+        const payload = {
+            vin: req.body.vin,
+            make: req.body.make,
+            model: req.body.model,
+            mileage: req.body.mileage,
+            transmissionType: req.body.transmissionType,
+            cleanTitle: req.body.cleanTitle,
+            salvage: req.body.salvage
+        }
+        await db("cars").where("id", req.params.id).update(payload)
+        const updatedCar = await db("cars").where("id", req.params.id).first()
+        res.json(updatedCar)
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+
 module.exports = router;
